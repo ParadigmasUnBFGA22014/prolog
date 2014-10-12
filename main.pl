64e2fc7227ffe0428f41e5643945aa0637bcc764
alexmesquita <alex.mesquita06@hotmail.com>
2014-10-12 18:04:07 -0300

    some recipes

--- main.pl
+++ main.pl
@@ -1,4 +1,3 @@
-main:-  consult('/Users/alissonnunes/Documents/workspace/prologParadigmas/receitas.pl'),
-	consult('/Users/alissonnunes/Documents/workspace/prologParadigmas/read_only_integer.pl'),
+main :-  consult('/home/alex/Projects/prolog/receitas.pl'),
+	consult('/home/alex/Projects/prolog/read_only_integer.pl'),
 	!.
-
